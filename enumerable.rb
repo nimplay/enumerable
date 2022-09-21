3dadf6bc9f2273a56fff52e2b72ec9f392b2d76e
nimplay <nimrod7day@gmail.com>
2022-09-21 09:49:39 -0400

    Add changes requirement

--- enumerable.rb
+++ enumerable.rb
@@ -1,57 +1,24 @@
 module MyEnumerable
-  def list
-    @list ||= []
-  end
-
   def all?
-    arr = []
-    list.each do |e|
-      arr.push(e) if yield e
+    each do |x|
+      return false unless yield(x)
     end
-    p arr.length == list.length
+    true
   end
 
   def any?
-    arr = []
-    list.each do |e|
-      arr.push(e) if yield e
+    each do |x|
+      return true if yield(x)
     end
-    p !arr.empty?
+    false
   end
 
   def filter
-    arr = []
-    list.each do |e|
-      arr.push(e) if yield e
+    filter_value = []
+    each do |x|
+      filter_value.push(x) if yield(x)
     end
-    p arr
-  end
-
-  def max
-    max_num = 0
-    list.each do |e|
-      e > max_num ? max_num = e : e
-    end
-    p max_num
-  end
-
-  def min
-    min_num = 999
-    list.each do |e|
-      e < min_num ? min_num = e : e
-    end
-    p min_num
-  end
-
-  def sort
-    arr = []
-    new_arr = list
-    list.length.times do
-      orig_arr = list
-      item_remove = orig_arr.min
-      arr.push(item_remove)
-      new_arr = orig_arr.delete(item_remove)
-    end
-    p arr
+    filter_value
   end
 end
+
--- list.rb
+++ list.rb
@@ -1,25 +1,30 @@
-require_relative 'enumerable'
+require_relative './enumerable'
 
 class MyList
   include MyEnumerable
-
   def initialize(*list)
     @list = list
   end
+
+  def each
+    (0...@list.length).each do |item|
+      yield @list[item]
+    end
+  end
 end
 
-# New list
 list = MyList.new(1, 2, 3, 4)
 
-# Test all?
-list.all? { |e| e > 5 }
+# Test #all?
 list.all? { |e| e < 5 }
-
-# Test any?
-list.any? { |e| e == 5 }
+# true
+list.all? { |e| e > 5 }
+# false
+# Test #any?
 list.any? { |e| e == 2 }
-
+# true
+list.any? { |e| e == 5 }
+# false
+# Test #filter
 list.filter(&:even?)
-list.sort
-list.max
-list.min
+# [2. 4]
