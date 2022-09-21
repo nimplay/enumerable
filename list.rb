b85d452eeba583823005fcb973efd771357c15e9
nimplay <nimrod7day@gmail.com>
2022-09-21 10:27:31 -0400

    last testing

--- list.rb
+++ list.rb
@@ -1,4 +1,26 @@
-require_relative './enumerable'
+module MyEnumerable
+  def all?
+    each do |x|
+      return false unless yield(x)
+    end
+    true
+  end
+
+  def any?
+    each do |x|
+      return true if yield(x)
+    end
+    false
+  end
+
+  def filter
+    filter_value = []
+    each do |x|
+      filter_value.push(x) if yield(x)
+    end
+    filter_value
+  end
+end
 
 class MyList
   include MyEnumerable
